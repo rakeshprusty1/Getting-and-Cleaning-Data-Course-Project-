475a9bfb78c7f287c9c7ddf52ce65245ef88a299
rakeshprusty1 <rakeshprusty@gmail.com>
2016-05-22 01:13:29 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,3 @@
-##########################################################################################################
-
-## Coursera Getting and Cleaning Data Course Project
-## Rakesh Prusty
-## 2016-05-21
-
-# runAnalysis.R File Description:
-
-# This script will perform the following steps on the UCI HAR Dataset downloaded from 
-# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-# 1. Merge the training and the test sets to create one data set.
-# 2. Extract only the measurements on the mean and standard deviation for each measurement. 
-# 3. Use descriptive activity names to name the activities in the data set
-# 4. Appropriately label the data set with descriptive activity names. 
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
 ##########################################################################################################
 
